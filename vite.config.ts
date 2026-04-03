bfeca93ae40a664f751ffd6f6296f7655fcf3b68
Ana Carolina <anaaraujo.dev@gmail.com>
2026-04-03 12:19:21 -0300

    feat: deploy files

--- vite.config.ts
+++ vite.config.ts
@@ -38,6 +38,7 @@ export default defineConfig({
             }
         })
     ],
+    base: '/massas-sao-jose-front/',
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
